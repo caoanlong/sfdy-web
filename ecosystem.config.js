ca5312551a101944f2ccdb62176d51fa493b565e
Aaron Cao <caoanlong@gmail.com>
2021-06-13 19:46:21 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'sfdy',
-            exec_mode: 'fork',
+            exec_mode: 'cluster',
             instances: 'max', // Or a number of instances
             script: 'yarn',
             args: 'start'
