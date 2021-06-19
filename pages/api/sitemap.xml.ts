7c1632ac47a756707a31e0cde3ae5b05c1ef7780
Aaron Cao <caoanlong@gmail.com>
2021-06-19 12:02:48 +0800

    opt

--- pages/api/sitemap.xml.ts
+++ pages/api/sitemap.xml.ts
@@ -1,14 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import fs from 'fs'
-import path from 'path'
 
 type Data = {
   name: string
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  console.log(__dirname)
-  const data = fs.readFileSync('/sitemap.xml', { encoding: 'utf-8' })
-  res.status(200).json({ data: data })
+
+  res.status(200).json({ data: 'data' })
 }
